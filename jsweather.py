e7e938d1f37b2afbf2b9e7b4557ef5ceb97a2cab
Q01 <8140841+pb8DvwQkfRR@users.noreply.github.com>
2023-07-20 13:31:25 +0800

    add photo upload
    Signed-off-by: Q01 <8140841+pb8DvwQkfRR@users.noreply.github.com>

--- jsweather.py
+++ jsweather.py
@@ -1,6 +1,7 @@
 import httpx
 import json
 import datetime
+import os
 from bs4 import BeautifulSoup
 
 
@@ -18,14 +19,14 @@ def jsweather_geturl():
     try:
         weatherurl = str('http://www.jsweather.com.cn:10000' + data["data"][0]["url"])
     except:
-        return
+        return None
     return weatherurl
 
 
 def jsweather_getall():
     url = jsweather_geturl()
     if url is None:
-        return "获取天气失败"
+        return "获取天气失败", [], []
     soup = BeautifulSoup(httpx.get(url), "html.parser")
     selectors = [
         ("p.Normal--Web-[style='line-height:38.0pt;text-indent:0.0pt;']", 0),
@@ -38,15 +39,20 @@ def jsweather_getall():
         ("span.Normal[style='color:#000000;font-size:14.0pt;font-family:宋体;']", 2),
     ]
     weather_text = ""
-    weather_img = ""
     weather_prefix = url.rsplit("/", 1)[0] + "/"
     weather_postfix = "#江苏天气 #江苏气象局 #短期天气预报"
-
+    weather_img_list = []
     for selector, index in selectors:
         elements = soup.select(selector)
         weather_text = weather_text + elements[index].text + "\n"
 
     img_tags = soup.find_all("img")
     for img in img_tags:
-        weather_img = weather_img + weather_prefix + img["src"] + "\n\n"
-    return weather_text + "\n" + weather_postfix + '\n' + weather_img
+        r = httpx.get(weather_prefix + img["src"], timeout=10)
+        with open('/tmp/a.png', 'wb') as f:
+            f.write(r.content)
+        files = {'uploadfile': (open('/tmp/a.png', 'rb'))}
+        r = httpx.post("https://temp.sh/upload", files=files)
+        os.remove('/tmp/a.png')
+        weather_img_list.append(r.text)
+    return weather_text, weather_postfix, weather_img_list
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 import os
 
-from telegram import Update
+from telegram import Update, InputMediaPhoto
+from telegram.constants import ParseMode
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 import get_hitfm
@@ -13,10 +14,22 @@ async def get_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 
 
 async def jsweather_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_html(jsweather.jsweather_getall())
+    weather_text, weather_postfix, weather_img_list = jsweather.jsweather_getall()
+    media_group = []
+    for i, image_url in enumerate(weather_img_list):
+        caption = weather_text + weather_postfix if i == 0 else None
+        media_group.append(
+            InputMediaPhoto(
+                media=image_url,
+                caption=caption,
+                parse_mode=ParseMode.HTML,
+            )
+        )
+    await update.message.reply_media_group(media=media_group)
 
 
 def main() -> None:
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.ERROR)
     token = os.environ.get("TG_TOKEN")
     try:
         if not token:
